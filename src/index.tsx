e3e1e4f0e69697306708de25f947c37f69dc54cd
brackenbit <brackenbit@proton.me>
2023-11-22 11:14:55 +1100

    Use npm installed bootstrap, react-bootstrap
    Install bootstrap, react-bootstrap, bootstrap-icons instead of using CDN.
    Update index.html, index.tsx to suit.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+// Import Bootstrap JS bundle
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Import Bootstrap icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
