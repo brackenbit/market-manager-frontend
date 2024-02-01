4990a69e7e5a1fc34608bb72f7c845a1a4340c77
brackenbit <brackenbit@proton.me>
2024-02-01 11:58:06 +1100

    Fix bug in useStallholderCategories. Was missing authState dependency, causing it to not re-trigger after logon on initial load.

--- src/CustomHooks/useStallholderCategories.tsx
+++ src/CustomHooks/useStallholderCategories.tsx
@@ -26,52 +26,51 @@ function useStallholderCategories() {
     const [httpErrorStallholderCategories, setHttpErrorStallholderCategories] =
         useState();
 
-    useEffect(() => {
-        const fetchStallholderCategories = async () => {
-            if (authState && authState.isAuthenticated) {
-                // Set up and make HTTP request
-                const url: string =
-                    "http://localhost:8080/api/stallholderCategories/search/findAllByOrderByNameAsc";
+    const fetchStallholderCategories = async () => {
+        if (authState && authState.isAuthenticated) {
+            setIsLoadingStallholderCategories(true);
 
-                const requestOptions = {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
-                        "Content-Type": "application/json",
-                    },
-                };
+            // Set up and make HTTP request
+            const url: string =
+                "http://localhost:8080/api/stallholderCategories/search/findAllByOrderByNameAsc";
 
-                const response = await fetch(url, requestOptions);
-                if (!response.ok) {
-                    throw new Error("Failed to fetch stallholder categories");
-                }
+            const requestOptions = {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                    "Content-Type": "application/json",
+                },
+            };
 
-                const responseJson = await response.json();
-                const responseData =
-                    responseJson._embedded.stallholderCategories;
-
-                // Load retrieved data into an array of StallholderCategoryModel
-                const loadedStallholderCategories: StallholderCategoryModel[] =
-                    [];
-                for (const key in responseData) {
-                    loadedStallholderCategories.push({
-                        id: responseData[key].id,
-                        name: responseData[key].name,
-                    });
-                }
-                setStallholderCategories(loadedStallholderCategories);
+            const response = await fetch(url, requestOptions);
+            if (!response.ok) {
+                throw new Error("Failed to fetch stallholder categories");
             }
 
-            setIsLoadingStallholderCategories(false);
-        };
+            const responseJson = await response.json();
+            const responseData = responseJson._embedded.stallholderCategories;
 
+            // Load retrieved data into an array of StallholderCategoryModel
+            const loadedStallholderCategories: StallholderCategoryModel[] = [];
+            for (const key in responseData) {
+                loadedStallholderCategories.push({
+                    id: responseData[key].id,
+                    name: responseData[key].name,
+                });
+            }
+            setStallholderCategories(loadedStallholderCategories);
+        }
+
+        setIsLoadingStallholderCategories(false);
+    };
+
+    useEffect(() => {
         fetchStallholderCategories().catch((error: any) => {
             // Error encountered - stop loading and set HTTP error
             setIsLoadingStallholderCategories(false);
             setHttpErrorStallholderCategories(error.message);
         });
-        window.scrollTo(0, 0);
-    }, []);
+    }, [authState]);
 
     return {
         stallholderCategories,
